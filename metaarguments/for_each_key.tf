3527ee94dbb00380b87793ea186f84f0d341bc4d
nagasai <nagasaikonderu@gmail.com>
2025-09-02 18:18:56 +0530

    ff

--- metaarguments/for_each_key.tf
+++ metaarguments/for_each_key.tf
@@ -3,6 +3,13 @@ provider "google" {
     region = "us-central1"
 }
 
+
+# variable "vm_names" {
+#     type = list(string)
+#     default = [ "nagasai1","nagasai2" ]
+  
+# }
+
 # resource "google_compute_instance" "tf_name1" {
 #     for_each = toset(var.vm_names)
 #     name = each.key
