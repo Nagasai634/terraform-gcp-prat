67bcc70a27584c9a9ceb265b57ba9ca2a6d0ebd1
nagasai <nagasaikonderu@gmail.com>
2025-09-03 15:53:03 +0530

    lifecycle2.0

---
+++ metaarguments/lifecycle/ignorechanges.tf
@@ -0,0 +1,33 @@
+provider "google" {
+    project = "emerald-spring-465406-s1"
+    region = "us-central1"
+}
+
+resource "google_compute_instance" "vm1"{
+    name = "vm1"
+    zone = "us-central1-a"
+    #zone = "us-central1-b"
+    machine_type = "e2-medium"
+    boot_disk {
+        initialize_params {
+          image = "centos-stream-9"
+        }
+    }
+    network_interface {
+        network = "default"
+        access_config {
+
+        }
+    }
+
+//with lifecycle(prevent_destroy ) : resource  will not get destroyed when
+    lifecycle {
+        prevent_destroy = false
+        create_before_destroy = false
+        ignore_changes = [ 
+           metadata_startup_script 
+         ]
+
+    }
+    metadata_startup_script = "echo hello from the terraform "
+}
--- metaarguments/lifecycle/preventdestroy.tf
+++ metaarguments/lifecycle/preventdestroy.tf
@@ -1,28 +1,29 @@
-provider "google" {
-    project = "emerald-spring-465406-s1"
-    region = "us-central1"
-}
+# provider "google" {
+#     project = "emerald-spring-465406-s1"
+#     region = "us-central1"
+# }
 
-resource "google_compute_instance" "vm1"{
-    name = "vm1"
-    zone = "us-central1-a"
-    #zone = "us-central1-b"
-    machine_type = "e2-medium"
-    boot_disk {
-        initialize_params {
-          image = "centos-stream-9"
-        }
-    }
-    network_interface {
-        network = "default"
-        access_config {
+# resource "google_compute_instance" "vm1"{
+#     name = "vm1"
+#     zone = "us-central1-a"
+#     #zone = "us-central1-b"
+#     machine_type = "e2-medium"
+#     boot_disk {
+#         initialize_params {
+#           image = "centos-stream-9"
+#         }
+#     }
+#     network_interface {
+#         network = "default"
+#         access_config {
 
-        }
-    }
+#         }
+#     }
 
-//with lifecycle(prevent_destroy ) : resource  will not get destroyed when
-    lifecycle {
-        prevent_destroy = true
-    }
+ //with lifecycle(prevent_destroy ) : resource  will not get destroyed when
+#     lifecycle {
+#        create_before_destroy = false
+#         prevent_destroy = true
+#     }
 
-}
+# }
